88c033d3df84788fb04f6f7ecddd2e302d3a2d00
A-BAI-A <uhsuiz@outlook.com>
2025-09-01 09:12:39 +0800

    Site updated: 2025-09-01 09:12:39

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1756688020685";const preCache = ["/images/taichi.png","/","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1756689146610";const preCache = ["/images/taichi.png","/","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
